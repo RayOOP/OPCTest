aa3cf3f9d64541355897383f274f7166fb1ee841
RayOOP <oopemail@gmail.com>
2022-12-31 14:22:21 +0800

    init test

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping
+    public String init(){
+        return "Hello world !!";
+    }
+}
